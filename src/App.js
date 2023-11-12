d1595bebc938d5320672f6a56f1ff1c11385f919
Ramadhan Rizqi Saputra <RamadhanRzq@gmail.com>
2023-11-12 20:49:05 +0700

    Add Type Animation

--- src/App.js
+++ src/App.js
@@ -1,7 +1,20 @@
+import fotoku from './assets/Foto.png'
+import { TypeAnimation } from 'react-type-animation';
+
 function App() {
   return (
-    <div>
-      
+    <div className='flex gap-96 justify-center items-center min-h-screen'>
+      <TypeAnimation sequence={[
+        'Ramadhan Rizqi Saputra',
+        1000, 
+        'Software Engineer',
+        1000,
+      ]}
+      speed={25}
+      style={{ fontSize: '2em'}}
+      repeat={2}
+      />
+      <img src={fotoku} alt="img" width={250} height={200} className='rounded-xl'/>
     </div>
   );
 }
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,8 +1,10 @@
+import '../App.css'
+
 function Navbar() {
   return (
-    <div className="flex justify-between p-6 bg-indigo-600 text-white">
+    <div className="flex justify-between p-6 bg-indigo-600 text-white ">
       <p className="text-lg">Ramadhan Rizqi Saputra</p>
-      <div className="flex gap-8">
+      <div className="flex gap-8 nav-menu">
         <a href="/">Home</a>
         <a href="/">Work</a>
         <a href="/">About</a>
