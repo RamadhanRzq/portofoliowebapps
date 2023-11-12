9a9a457941f4a956c40f539d786d48b4e52ef74e
Ramadhan Rizqi Saputra <RamadhanRzq@gmail.com>
2023-11-12 20:17:18 +0700

    Add Navbar

---
+++ src/components/Navbar/Navbar.js
@@ -0,0 +1,15 @@
+function Navbar() {
+  return (
+    <div className="flex justify-between p-6 bg-indigo-600 text-white">
+      <p className="text-lg">Ramadhan Rizqi Saputra</p>
+      <div className="flex gap-8">
+        <a href="/">Home</a>
+        <a href="/">Work</a>
+        <a href="/">About</a>
+        <a href="/">Contact</a>
+      </div>
+    </div>
+  );
+}
+
+export default Navbar;
