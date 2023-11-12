400cabf93c46741c9f1b9907d3b54be2f31215e2
Ramadhan Rizqi Saputra <RamadhanRzq@gmail.com>
2023-11-12 21:05:42 +0700

    Add Container as Card

--- src/App.js
+++ src/App.js
@@ -1,20 +1,33 @@
-import fotoku from './assets/Foto.png'
-import { TypeAnimation } from 'react-type-animation';
+import fotoku from "./assets/Foto.png";
+import { TypeAnimation } from "react-type-animation";
 
 function App() {
   return (
-    <div className='flex gap-96 justify-center items-center min-h-screen'>
-      <TypeAnimation sequence={[
-        'Ramadhan Rizqi Saputra',
-        1000, 
-        'Software Engineer',
-        1000,
-      ]}
-      speed={25}
-      style={{ fontSize: '2em'}}
-      repeat={2}
-      />
-      <img src={fotoku} alt="img" width={250} height={200} className='rounded-xl'/>
+    <div className="flex justify-center items-center min-h-screen">
+      <div className="container flex justify-center gap-96 items-center bg-white rounded-lg shadow-md p-4">
+        <div className="flex flex-col gap-2">
+          <TypeAnimation
+            sequence={[
+              "Ramadhan Rizqi Saputra",
+              1000,
+              "Software Engineer.",
+              1000,
+            ]}
+            speed={25}
+            style={{ fontSize: "26px" }}
+            repeat={2}
+          />
+          <p>Freelance Web Developer</p>
+          <p>Institut Teknologi Telkom Purwokerto</p>
+        </div>
+        <img
+          src={fotoku}
+          alt="img"
+          width={250}
+          height={200}
+          className="rounded-xl p-2"
+        />
+      </div>
     </div>
   );
 }
